c6e7e8fdb4716bbf624d7da98b8e754bc29b30ab
Soren Nissen <s.nissen.bass@gmail.com>
2020-10-06 18:53:02 +0000

    added jquery - charcounting functionality and CSS styling for negative values

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('keyup', function() {
+    let charCount = $(this).val().length;
+    let charRemaining = 140 - charCount;
+    console.log(charCount);
+    console.log(charRemaining);
+
+    $(this).siblings('div').children('.counter').text(charRemaining);
+    if (charRemaining < 0) {
+      $(this).siblings('div').children('.counter').addClass('negative');
+    } else {
+      $(this).siblings('div').children('.counter').removeClass('negative');
+    }
+  });
+
+
+});
+
+// charCount($(this).val().length)
